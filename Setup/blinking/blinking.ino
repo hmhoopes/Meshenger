52f0bf50e6918c606d781aa97394b60f15d532b6
Michael Hoopes <hmhoopes@gmail.com>
2026-02-11 14:51:35 -0600

    Add setup tutorial

---
+++ Setup/blinking/blinking.ino
@@ -0,0 +1,12 @@
+int ledPin = 2; // Default In-Built LED pin for ESP32 Dev Module
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn the LED on
+  delay(1000);               // Wait for 1 second
+  digitalWrite(ledPin, LOW); // Turn the LED off
+  delay(1000);               // Wait for 1 second
+}
